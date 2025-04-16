3ef8fd154696cc58d2a4298b41a7bd9950f2b1d8
HafizBkr <hafizinovus@gmail.com>
2025-04-16 10:36:17 +0000

    commit for my fist C# console_APP

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+     static void Main(string[] args)
+    {
+        int resultat=Functions.Addition(5, 10);
+        Console.WriteLine("Le resultat est: " + resultat);
+    }
+}
---
+++ functions.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Functions{
+    public static int Addition (int a, int b){
+        return a + b;
+    }   
+    private static int Soustraction (int a, int b){
+        return a - b;
+    }
+}
